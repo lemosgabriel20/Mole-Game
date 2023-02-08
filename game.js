393c451a482bc921258e71a22aca509eca10f01d
Gabriel Lemos <58190014+lemosgabriel20@users.noreply.github.com>
2023-02-08 16:21:34 -0300

    dELETADOO GAME.JS

--- game.js
+++
@@ -1,3 +0,0 @@
-import { play } from "./play.js";
-
-play()
--- play.js
+++ play.js
@@ -1,34 +1,28 @@
-export const play = () => {
-    // instancias
+
     const moleImg = new Image();
     const buracoImg = new Image();
-    
-    // DOM selector
     const canvas = document.querySelector('#myCanvas');
     const ctx = canvas.getContext("2d");
     const scoreText = document.querySelector('#scoreText');
+    const menuReturn = document.querySelector('#menu_return')
+    const overlay = document.querySelector('.overlay')
     const body = document.querySelector('body');
-    
-    // import
-    // canvas.style.backgroundImage = "url(../images/grass.png)";
-    // document.documentElement.style.cursor = "url(../images/hammerIdle.png), auto";
+    const easy = document.querySelector('#easy_button')
+    const medium = document.querySelector('#medium_button')
+    const hard = document.querySelector('#hard_button')
+    let difficulty = 1500;
     buracoImg.src = "../images/buraco.png";
     moleImg.src = "../images/toupeirinha.png";
-    
-    // variáveis
+    const playButton = document.querySelector('#play')
     const buracos = 9;
     const initialPosX = 40;
     const initialPosY = 80;
     const sizeX = 50;
     const sizeY = 50;
-    const displayMoleInterval = 1500;
-    const clearMoleInterval = displayMoleInterval - 500;
-    // const canvasWidth = canvas.offsetWidth;
-    // const canvasHeight = canvas.offsetHeight;
+    let displayMoleInterval = difficulty;
+    let clearMoleInterval = displayMoleInterval - 500;
     const toupeiraOffset = 5;
     const positions = {}
-    
-    // inicializadores
     let offsetX = 230;
     let offsetY = 120;
     let score = 0;
@@ -37,71 +31,92 @@ export const play = () => {
     let currentPosX = initialPosX;
     let currentPosY = initialPosY;
     let globalMolePosition = {};
-    
-    // ctx generico
-    const setPath = (color, x, y, width, height) => {
-      ctx.beginPath();
-      ctx.rect(x, y, width, height);
-      ctx.fillStyle = color;
-      ctx.fill();
-      ctx.closePath();
-    }
-    
-    // cria o campo do jogo
+    let gameInterval = null;
     const createField = () => {
-       for (let i = 0; i < 3; i += 1) {
-        ctx.drawImage(buracoImg, currentPosX += 20, currentPosY += 20);
-        positions[index] = { 
-          initialX: currentPosX, 
-          initialY: currentPosY, 
-          finalX: currentPosX + sizeX, 
-          finalY: currentPosY + sizeY 
-        };
-        index += 1;
-      
-        // criação dos buracos verticais
-        for (let j = 0; j < 2; j += 1) {
-          currentPosY += offsetY;
-          ctx.drawImage(buracoImg, currentPosX, currentPosY);
-          positions[index] = { 
-            initialX: currentPosX, 
-            initialY: currentPosY, 
-            finalX: currentPosX + sizeX, 
-            finalY: currentPosY + sizeY 
-          };
-          index += 1;
-        }
-        currentPosY =  initialPosY;
-        currentPosX += offsetX;
-      }
-      canvas.style.backgroundImage = "url(../images/grass.png)";
-    }
-    
-    setTimeout(() => {
-      createField()
-    }, 100)
-    
-    // randomiza a toupeira nos buracos
+      for (let i = 0; i < 3; i += 1) {
+       ctx.drawImage(buracoImg, currentPosX += 20, currentPosY += 20);
+       positions[index] = { 
+         initialX: currentPosX, 
+         initialY: currentPosY, 
+         finalX: currentPosX + sizeX, 
+         finalY: currentPosY + sizeY 
+       };
+       index += 1;
+     
+       for (let j = 0; j < 2; j += 1) {
+         currentPosY += offsetY;
+         ctx.drawImage(buracoImg, currentPosX, currentPosY);
+         positions[index] = { 
+           initialX: currentPosX, 
+           initialY: currentPosY, 
+           finalX: currentPosX + sizeX, 
+           finalY: currentPosY + sizeY 
+         };
+         index += 1;
+       }
+       currentPosY =  initialPosY;
+       currentPosX += offsetX;
+     }
+     canvas.style.backgroundImage = "url(../images/grass.png)";
+   }
+
+
+
+    const startGame = () => {
+
     const setMolePosition = () => {
       const index =  Math.floor(Math.random() * 9 + 1);
       return positions[index];
     }
-    
-    // spawna a topeira
-    setInterval((() => {
+
+     gameInterval = setInterval((() => {
       const { initialX, initialY, finalX, finalY } = setMolePosition();
       globalMolePosition = { initialX, initialY, finalX, finalY };
       ctx.drawImage(moleImg, initialX + 2.5, initialY - 16);
       const timeoutID = setTimeout((() => {
-        createField()
+        //createField()
         ctx.clearRect(initialX, initialY - 20, 100, 140)
         ctx.drawImage(buracoImg, initialX, initialY);
         globalMolePosition = {}
         clearTimeout(timeoutID)
       }), clearMoleInterval);
     }), displayMoleInterval);
+  }
+  playButton.addEventListener('click', () => {
+    currentPosX = initialPosX;
+    currentPosY = initialPosY;
+    displayMoleInterval = difficulty;
+    clearMoleInterval = displayMoleInterval - 500;
+    createField();
+    startGame();
+    overlay.style.display = 'none'
+    playButton.style.display = 'none'
+  })
+    const clearDisplay = () => {
+      canvas.style.backgroundImage = "none";
+      clearInterval(gameInterval);
+      ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+    }
+
+    menuReturn.addEventListener('click', () => {
+      clearDisplay();
+      overlay.style.display = 'flex'
+      playButton.style.display = 'flex'
+    })
+
+    easy.addEventListener('click', () => {
+      difficulty = (1500)
+    })
     
-    // conta a pontuação por clique
+    medium.addEventListener('click', () => {
+      difficulty = (1100)
+    })
+    
+    hard.addEventListener('click', () => {
+      difficulty = (900)
+    })
+  
+
     canvas.addEventListener('click', (evt) => {
       const rect = canvas.getBoundingClientRect();
       const mouseX = evt.clientX - rect.left;
@@ -111,19 +126,7 @@ export const play = () => {
           && mouseY >= initialY - 16 && mouseY <= finalY
         ) {
           score += 1;
-          // Para evitar farm de pontos, reseta a posição CONHECIDA da toupeira pelo mouse
           globalMolePosition = {};
           scoreText.innerHTML = score;
         }
-      });
-      
-    // sprite do cursor
-    /*
-    body.addEventListener('mousedown', () => {
-      body.style.cursor = "url(../images/hammerMove.png), auto";
-    })
-    
-    body.addEventListener('mouseup', () => {
-      body.style.cursor = "url(../images/hammerIdle.png), auto";
-    })*/
-    }
+    });
